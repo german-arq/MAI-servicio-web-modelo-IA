2afef4759fa39c69edb25569304c24c39dda35cc
German Rojas <germanlmg@gmail.com>
2022-11-25 19:10:23 -0500

    Corrige la invocacion del endpoint por tipo de dato csv

--- app.py
+++ app.py
@@ -34,22 +34,28 @@ def prediccion(data):
 
     bien_data = [float(formated_data.get('Area')), float(formated_data.get('Perimetro')), int(formated_data.get('Cantidad de vertices')), float(formated_data.get('Nivel')), float(formated_data.get('RMC Largo')), float(formated_data.get('RMC Alto')), float(formated_data.get('RMC Ratio'))]
 
+    bien_data_for_endpoint = ",".join(str(x) for x in bien_data)
+    bien_data_for_endpoint = bien_data_for_endpoint + "\n" + bien_data_for_endpoint
+
+    print(bien_data)
+    print(bien_data_for_endpoint)
+
     # Predicción con Endpoint SageMaker
     endpoint = 'sagemaker-scikit-learn-2022-11-25-23-15-58-775'
     client = boto3.client('sagemaker-runtime', 'us-east-1')
 
-    response = client.invoke_endpoint(EndpointName=endpoint, Body=json.dumps(bien_data), ContentType='application/json')
+    response = client.invoke_endpoint(EndpointName=endpoint, Body=bien_data_for_endpoint, ContentType='text/csv')
 
-    print(response)
+    #print(response)
 
-    prediccion = json.loads(response['Body'].read().decode())
+    # Devuelve un string con el valor de la predicción, entonces extraemos el segundo caracter que contiene el valor de la predicción y lo convertimos a entero
+    prediccion = int(response['Body'].read().decode()[1:2])
+
 
     print(prediccion)
 
-
-
     # Predicción con modelo local
-    prediccion = model.predict([bien_data])
+    #prediccion = model.predict([bien_data])
 
 
     complemento = f"El Bien con características: {bien_data} es"
